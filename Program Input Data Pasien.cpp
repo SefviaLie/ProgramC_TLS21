7c3a01810afd47de83b5e6419d30b3a7e500bc00
SefviaLie <92975595+SefviaLie@users.noreply.github.com>
2021-10-22 14:50:05 +0700

    Add files via upload

---
+++ Program Input Data Pasien.cpp
@@ -0,0 +1,43 @@
+//Program Menginput Data Pasien
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string Name;
+    int Age;
+    string Gender;
+    string Blood_Type;
+    int HealthID;
+    int Phone;
+    int Emergency_contact;
+    
+    cout << "                             Welcome To DTETI Hospital\n";
+    cout << "Please enter your data...\n";
+    cout << "Enter Name               : ";
+    getline(cin,Name);
+    cout << "Enter Age                : ";
+    cin >> Age;
+    cout << "Enter Gender             : ";
+    cin >> Gender;
+    cout << "Enter Blood Type         : ";
+    cin >> Blood_Type;
+    cout << "Enter Health ID          : ";
+    cin >> HealthID;
+    cout << "Enter Phone Number       : ";
+    cin >> Phone;
+    cout << "Enter Emergency Contact  : ";
+    cin >> Emergency_contact;
+    
+    cout << "\nPlease re-check your data : ";
+    cout << "\n      ---Patient Data---\n";
+    cout << "Name              : " << Name << endl;
+    cout << "Age               : " << Age << endl;
+    cout << "Gender            : " << Gender << endl;
+    cout << "Blood Type        : " << Blood_Type << endl;
+    cout << "Health ID         : " << HealthID << endl;
+    cout << "Phone             : +62" << Phone << endl;
+    cout << "Emergency Contact : +62" << Emergency_contact << endl;
+    
+    return 0;
+}
